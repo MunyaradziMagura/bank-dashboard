9cd13e858555b471843d4382bdc98870c4db8821
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-10-07 18:49:51 +1030

    added react project and able to call data data from upbank api

---
+++ dashboard/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import Server from './components/Server'
+function App() {
+  async function UpBank(_token, _endpoint){
+    var connection = await new Server(_token,_endpoint);
+
+    // get data 
+    var data = await connection.getUpData()
+    // get accounts
+    var result = await connection.accounts(data)
+    console.log(result)
+  }
+  
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard/src/components/Server.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export default class Server {
+    // set token 
+    constructor(_token, _endpoint) {
+        this.UP_TOKEN = _token;
+        this.endpoint = _endpoint // "/accounts" 
+        this.baseURL = "https://api.up.com.au/api/v1"
+      }
+    // get token
+    getToken() {
+        return this.UP_TOKEN
+    }
+    // get json full data
+    getUpData = async () => {
+        var result = await axios.get(this.baseURL + this.endpoint, {headers: {Authorization: 'Bearer ' + this.UP_TOKEN}})
+        // console.log(result.data)
+        return result.data.data
+    }
+    // get accounts
+    accounts = async (_data) => {
+        let data = await _data
+        let accounts = []
+        data.forEach(account => {
+          accounts.push(account.attributes.displayName)
+        })
+        return accounts
+      }
+
+}
