279aa6addf31cb592ba1cbac30bbab4b79feaeb1
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-11-22 14:40:09 +1030

    cleaned up project of unused files then added a dynamic input box which fetches a list of user accounts

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,42 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 import Server from './components/Server'
-
 function App() {
+  const [id, setID] = useState();
 
   async function UpBank(_token, _endpoint){
     var connection = await new Server(_token,_endpoint);
 
     // get data 
     var data = await connection.getUpData()
+    console.log(data)
     // get accounts
     var result = await connection.accounts(data)
+    console.log(result)
   }
-  console.log(process.env.UP_TOKEN)
 
-  UpBank(process.env.UP_TOKEN, '/accounts')
+  useEffect(() => {
+    UpBank(id, '/accounts')
+
+  },[id])
   
   
-
-
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container text-center">
+      <div className="row">
+      <nav className="navbar bg-light">
+    {/* navigation bar */}
+      <div className="container-fluid">
+        <a className="navbar-brand">Up Account</a>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Enter Up API code" aria-label="Search" onChange={(e) => setID(e.target.value) }/>
+        </form>
+      </div>
+    </nav>
+      </div>
+      <div className="row">
+      <div className="col">
+      1 of 3
     </div>
+    <div className="col-8">
+
+
+
+      
+    </div>
+    <div className="col">
+      3 of 3
+    </div>
+      </div>
+    </div>
+    
   );
 }
 
