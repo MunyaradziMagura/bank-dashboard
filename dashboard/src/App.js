0402e0bfe877c6e065791ffced73c5adb7f82547
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-11-22 15:41:04 +1030

    can dynamically render and generate account cards

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import {useState, useEffect} from 'react'
 import Server from './components/Server'
+import Accounts from './components/Accounts';
 function App() {
   const [id, setID] = useState();
-
+  const [accountsList, setAccountsList] = useState(["none"])
   async function UpBank(_token, _endpoint){
-    var connection = await new Server(_token,_endpoint);
+    const connection = await new Server(_token,_endpoint);
 
     // get data 
-    var data = await connection.getUpData()
-    console.log(data)
+    const data = await connection.getUpData()
     // get accounts
-    var result = await connection.accounts(data)
-    console.log(result)
+    const getAccounts = await connection.accounts(data)
+    setAccountsList(getAccounts)
   }
 
   useEffect(() => {
@@ -29,7 +29,7 @@ function App() {
       <nav className="navbar bg-light">
     {/* navigation bar */}
       <div className="container-fluid">
-        <a className="navbar-brand">Up Account</a>
+        <h1 className="navbar-brand">Up Account</h1>
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Enter Up API code" aria-label="Search" onChange={(e) => setID(e.target.value) }/>
         </form>
@@ -37,19 +37,30 @@ function App() {
     </nav>
       </div>
       <div className="row">
-      <div className="col">
-      1 of 3
-    </div>
-    <div className="col-8">
+      <div className="col"/>
+      {/* main page content */}
+    <div className="col-12">
 
+    <div className="row gy-3">
+    <div className="col-12">
+    {/* generates cards for each account */}
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+      {accountsList.map(account => (
+        <Accounts account={account}/>
+      ))}
+    </div>
+
+    </div>
+    <div className="col-12">
+      <div className="p-3 border bg-light">account content</div>
+    </div>
+    </div>
 
 
       
     </div>
-    <div className="col">
-      3 of 3
+    <div className="col"/>
     </div>
-      </div>
     </div>
     
   );
--- dashboard/src/components/Accounts.js
+++ dashboard/src/components/Accounts.js
@@ -6,12 +6,9 @@ export default function Accounts({account}, props) {
   return (
     <div className="col">
     <div className="card h-100 bg-secondary">
-
    <MyLogo/>
   <div className="card-img-overlay">
-    <h1 className="card-title ">{account}</h1>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+    <h1 className="card-title text-warning">{account}</h1>
   </div>
     </div>
   </div>
